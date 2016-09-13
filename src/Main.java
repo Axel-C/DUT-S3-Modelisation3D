ed1d2184e69c56376f41013175b3fd1524a34132
Samson VASSEUR <vasseur.samson@gmail.com>
2016-09-13 11:20:46 +0200

    modifié :         src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
 public class Main {
-<<<<<<< HEAD
-	//Test Samson
-	
 
-=======
 	public static void main(String[] args) {
-		
+		//test Samson
 	}
->>>>>>> 58d851709b03d5e3044866e0272f5d8d0aab4f16
 }
