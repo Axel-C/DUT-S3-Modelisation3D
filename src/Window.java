90dc04429f8ca86ff3cf7eb939336ab039d3e965
Melissa MAERTEN <melissa.maerten@etudiant.univ-lille1.fr>
2016-09-13 11:53:20 +0200

    Ajout JMenu (en commentaire) + Ajout d' un répertoire tests

--- src/Window.java
+++ src/Window.java
@@ -1,10 +1,19 @@
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class Window extends JFrame {
 	
 	public Window(){
 		super("Projet Modelisation");
 		setSize(500, 500);
+		
+		/*JMenuBar menu= new JMenuBar();
+		JMenu fichier = new JMenu("Fichier");
+		menu.add(fichier);
+		setJMenuBar(menu);*/
+				
 		
 		
 		
