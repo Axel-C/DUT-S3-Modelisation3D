4cfcb8fae86627ade07e4e6a6b23c7bffd994ad8
Axel COEURET <axel.coeuret@etudiant.univ-lille1.fr>
2016-09-13 11:28:15 +0200

    Ajout de Interface

---
+++ src/Interface.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Interface extends JFrame {
+	
+	public Interface(){
+		super("Projet Modelisation");
+		setSize(500, 500);
+		
+		
+		
+		setVisible(true);
+	}
+}
