801643bb107c293f6835373da1be0952bf5a7b7f
Samson VASSEUR <vasseur.samson@gmail.com>
2016-09-20 11:42:57 +0200

    Ajout du test de la présence des lignes "element vertex" et "element face" dans le fichier ply

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,19 @@ import java.io.File;
 
 public class Main {
 	private static MyJFrame jFrame;
+
 	public static void main(String[] args) {
-		jFrame = new MyJFrame(new ThreeDimensionalSpace(new File("livrable1/data/test_file1.ply")));
-		for(int i = 0; i < jFrame.getThreeDimensionalSpace().vertices.length; i++) {
-			System.out.println(jFrame.getThreeDimensionalSpace().vertices[i]);
-		}
-		
-		for(int i = 0; i < jFrame.getThreeDimensionalSpace().faces.length; i++) {
-			System.out.println(jFrame.getThreeDimensionalSpace().faces[i]);
+
+		if (GoodFormatPLY.check(new File("livrable1/data/test_file2.ply"))) {
+
+			jFrame = new MyJFrame(new ThreeDimensionalSpace(new File("livrable1/data/test_file1.ply")));
+			for (int i = 0; i < jFrame.getThreeDimensionalSpace().vertices.length; i++) {
+				System.out.println(jFrame.getThreeDimensionalSpace().vertices[i]);
+			}
+
+			for (int i = 0; i < jFrame.getThreeDimensionalSpace().faces.length; i++) {
+				System.out.println(jFrame.getThreeDimensionalSpace().faces[i]);
+			}
 		}
 	}
 }
