8103bd48fee6d2aa6e64099024b1257dd70d8b0e
Melissa MAERTEN <melissa.maerten@etudiant.univ-lille1.fr>
2016-09-20 11:13:33 +0200

    Ajout d' un Test : vérification extension du fichier à ouvrir

--- test/ThreeDimensionalSpaceTest.java
+++ test/ThreeDimensionalSpaceTest.java
@@ -1,3 +1,30 @@
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
 public class ThreeDimensionalSpaceTest {
+	File plyFile= new File("livrable1/data/test_file1.ply");
+	File plyFileTxt= new File("livrable1/data/test_file1.txt");
+	ThreeDimensionalSpace s=new ThreeDimensionalSpace(plyFile);
 	
+	private boolean ouvrable(File file) {
+		String ext="";
+		if (file.getName().lastIndexOf(".") > 0) {
+			ext = file.getName().substring(file.getName().lastIndexOf("."));
+		}
+		return ext.equals(".ply");
+	}
+	
+	@Test
+	public void testOuvrable() {
+		assertTrue(ouvrable(plyFile));
+		assertFalse(ouvrable(plyFileTxt));
+	}
+
 }
