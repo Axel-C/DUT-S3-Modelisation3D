ccf853cfb71c248e880c06368e6fc60b5ef7f4a8
axelc <axelc@192.168.1.91>
2016-10-09 15:03:14 +0200

    compareTo pour Face

--- src/Face.java
+++ src/Face.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class Face {
+public class Face implements Comparable<Face>{
 	private Vertex[] vertices;
 
 	public Face(Vertex[] vertices) {
@@ -15,5 +15,23 @@ public class Face {
 	public String toString() {
 		return "Face [vertices=" + Arrays.toString(vertices) + "]";
 	}
+	
+	public double getMaximumZ(){
+		double max = vertices[0].getZ() ;
+		 for(int i = 1 ; i < vertices.length ; i++){
+			 if(vertices[i].getZ() > max ){
+				 max = vertices[i].getZ();
+			 }
+		 }
+		return max ;
+	}
+
+	@Override
+	public int compareTo(Face face) {
+		double z1 = this.getMaximumZ();
+		double z2 = face.getMaximumZ();
+		return (int) (z1 - z2) ;
+	}
+	
 
 }
