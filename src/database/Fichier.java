47663cd7ec6c427a2ba112bc5d9e712464200552
Axel COEURET <axel.coeuret@etudiant.univ-lille1.fr>
2016-10-25 14:59:31 +0200

    IHM + base de donnée

---
+++ src/database/Fichier.java
@@ -0,0 +1,35 @@
+ package database;
+ 
+ import java.awt.Dimension;
+ 
+ import javax.swing.JLabel;
+	
+ import javax.swing.JPanel;
+ 
+ @SuppressWarnings("serial")
+ public class Fichier extends JPanel{
+ 	private int id ;
+ 	public String nom ; 
+ 	public String contenu ;
+ 	public int points ;
+ 	public int segments ;
+ 	public int faces ;
+ 	
+ 	public Fichier(int id , String nom , String contenu , int points , int segments , int faces ){
+ 		this.id = id ;
+ 		this.nom = nom ;
+ 		this.contenu = contenu ;
+ 		this.points = points ;
+ 		this.segments = segments ;
+ 		this.faces = faces ;
+ 		add(new JLabel(nom));
+ 		setMaximumSize(new Dimension(200, 20));
+ 	}
+ 	
+ 	public String toString(){
+ 		return "[" + id + " , " + nom + " , " +points + " , " + segments + " , " + faces + "]" ;
+ 	}
+ 	
+ 	
+ 	
+ }
---
+++ src/database/Test.java
@@ -0,0 +1,11 @@
+ package database;
+ 
+	
+ public class Test {
+ 
+ 	public static void main(String[] args) {
+ 		new Menu();
+ 
+ 	}
+ 
+ }
