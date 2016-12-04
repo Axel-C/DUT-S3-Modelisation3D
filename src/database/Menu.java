60df83aff1d53b083b5199627baf38c334aec1c7
remim <remim@DESKTOP-4L96LU3>
2016-12-04 18:16:29 +0100

    ajout des jars et petit nettoyage

--- src/database/Menu.java
+++ src/database/Menu.java
@@ -5,14 +5,12 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-import javafx.geometry.Orientation;
-
+@SuppressWarnings("serial")
 public class Menu extends JFrame {
 	public static final int AJOUTER = 0 ;
 	public static final int MODIFIER = 1 ;
